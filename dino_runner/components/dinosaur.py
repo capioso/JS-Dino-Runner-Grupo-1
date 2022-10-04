b4052bd00e9fce01e6a142252fb10a0d611cd329
capioso <a.h.segales741@gmail.com>
2022-10-04 11:07:58 -0400

    Homework Class 01 - Dino_ducking

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,13 +1,14 @@
 import pygame
 from pygame.sprite import Sprite
 
-from dino_runner.utils.constants import JUMPING, RUNNING
+from dino_runner.utils.constants import JUMPING, RUNNING, DUCKING
 
 
 class Dinosaur(Sprite):
     X_POS = 80
     Y_POS = 310
     JUMP_VELOCITY = 8.5
+    DUCK_VELOCITY = 8.5
 
     def __init__(self):
         self.image = RUNNING[0]
@@ -18,19 +19,30 @@ class Dinosaur(Sprite):
 
         self.dino_run = True
         self.dino_jump = False
+        self.dino_duck = False
         self.jump_velocity = self.JUMP_VELOCITY
+        self.duck_velocity = self.DUCK_VELOCITY
 
     def update(self, user_input):
         if self.dino_run:
             self.run()
         elif self.dino_jump:
             self.jump()
+        elif self.dino_duck:
+            self.duck()
 
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
         elif not self.dino_jump:
             self.dino_jump = False
+            self.dino_run = True
+
+        if user_input[pygame.K_DOWN]:
+            self.dino_duck = True
+            self.dino_run = False
+        elif not self.dino_duck:
+            self.dino_duck = False
             self.dino_run = True
 
         if self.step_index >= 10:
@@ -41,8 +53,8 @@ class Dinosaur(Sprite):
         self.dino_rect.y -= self.jump_velocity * 4
         self.jump_velocity -= 0.8
 
-        print("Jump Velocity", self.jump_velocity)
-        print("Y POS: ", self.dino_rect.y)
+        print("Jump velocity:", self.jump_velocity)
+        print("Y Position:", self.dino_rect.y)
 
         if self.jump_velocity < -self.JUMP_VELOCITY:
             self.dino_jump = False
@@ -57,7 +69,9 @@ class Dinosaur(Sprite):
         self.step_index += 1
 
     def duck(self):
-        pass #cuando se presione tecla hacia abajo que se agache
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect.y = 340
+        self.step_index += 1
 
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
