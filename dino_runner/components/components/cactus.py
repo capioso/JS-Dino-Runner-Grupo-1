fc366b2788de6559d0311441c003ff1f4e154ea3
capioso <a.h.segales741@gmail.com>
2022-10-04 20:11:56 -0400

    Class 02: Obstacles added
    Just small cactus added

---
+++ dino_runner/components/components/cactus.py
@@ -0,0 +1,11 @@
+import imp
+
+
+import random
+from .obstacle import Obstacle
+
+class Cactus(Obstacle):
+    def __init__(self, images):
+        type = random.randint(0,2)
+        super().__init__(images,type)
+        self.rect.y = 325
--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -7,8 +7,9 @@ from dino_runner.utils.constants import JUMPING, RUNNING, DUCKING
 class Dinosaur(Sprite):
     X_POS = 80
     Y_POS = 310
+    Y_POS_DUCK = 340
     JUMP_VELOCITY = 8.5
-    DUCK_VELOCITY = 8.5
+    
 
     def __init__(self):
         self.image = RUNNING[0]
@@ -21,40 +22,37 @@ class Dinosaur(Sprite):
         self.dino_jump = False
         self.dino_duck = False
         self.jump_velocity = self.JUMP_VELOCITY
-        self.duck_velocity = self.DUCK_VELOCITY
+        
 
     def update(self, user_input):
         if self.dino_run:
             self.run()
-        elif self.dino_jump:
-            self.jump()
         elif self.dino_duck:
             self.duck()
+        elif self.dino_jump:
+            self.jump()
 
-        if user_input[pygame.K_UP] and not self.dino_jump:
-            self.dino_jump = True
-            self.dino_run = False
-        elif not self.dino_jump:
-            self.dino_jump = False
-            self.dino_run = True
+        if not self.dino_jump:
+            if user_input[pygame.K_UP]: #evita que se superpongan las ordenes, no salta si ya esta saltando
+                self.dino_jump = True
+                self.dino_duck = False
+                self.dino_run = False
+            elif user_input[pygame.K_DOWN]: #evita que se superponga las ordenes, no agacha mientras salta
+                self.dino_duck = True
+                self.dino_jump = False
+                self.dino_run = False
+            else:
+                self.dino_jump = False
+                self.dino_duck = False
+                self.dino_run = True
 
-        if user_input[pygame.K_DOWN]:
-            self.dino_duck = True
-            self.dino_run = False
-        elif not self.dino_duck:
-            self.dino_duck = False
-            self.dino_run = True
-
-        if self.step_index >= 10:
+        if self.step_index >= 9:
             self.step_index = 0
 
     def jump(self):
         self.image = JUMPING
         self.dino_rect.y -= self.jump_velocity * 4
         self.jump_velocity -= 0.8
-
-        print("Jump velocity:", self.jump_velocity)
-        print("Y Position:", self.dino_rect.y)
 
         if self.jump_velocity < -self.JUMP_VELOCITY:
             self.dino_jump = False
@@ -69,8 +67,10 @@ class Dinosaur(Sprite):
         self.step_index += 1
 
     def duck(self):
-        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
-        self.dino_rect.y = 340
+        self.image = DUCKING[self.step_index // 5]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = self.X_POS
+        self.dino_rect.y = self.Y_POS_DUCK
         self.step_index += 1
 
     def draw(self, screen):
